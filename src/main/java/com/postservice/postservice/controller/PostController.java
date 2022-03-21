f683411f0a927874ab6e9c65904bd2fe8a9d3682
senil <senila@maveric-systems.com>
2022-03-21 23:54:11 +0530

    find by id

--- src/main/java/com/postservice/postservice/controller/PostController.java
+++ src/main/java/com/postservice/postservice/controller/PostController.java
@@ -1,11 +1,23 @@
 package com.postservice.postservice.controller;
 
+import com.postservice.postservice.model.Post;
 import com.postservice.postservice.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PostController {
     @Autowired
     private PostService postService;
+
+
+    @GetMapping("/posts/{postId}")
+    public ResponseEntity<Post> findById(@PathVariable("postId") String postId){
+        return new ResponseEntity<>(postService.findById(postId), HttpStatus.ACCEPTED);
+    }
+
 }
--- src/main/java/com/postservice/postservice/service/PostService.java
+++ src/main/java/com/postservice/postservice/service/PostService.java
@@ -1,5 +1,6 @@
 package com.postservice.postservice.service;
 
+import com.postservice.postservice.model.Post;
 import com.postservice.postservice.repo.PostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,9 @@ public class PostService {
 @Autowired
     private PostRepo postRepo;
 
+    public Post findById(String postId){
+        return this.postRepo.findById(postId).get();
+    }
 
 
 
