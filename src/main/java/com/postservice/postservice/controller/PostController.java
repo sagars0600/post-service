e0e89a8a9fb9b88ee2f356edcc0e2a1149e6ea04
senil <senila@maveric-systems.com>
2022-03-22 00:14:24 +0530

    update post

--- src/main/java/com/postservice/postservice/controller/PostController.java
+++ src/main/java/com/postservice/postservice/controller/PostController.java
@@ -1,11 +1,24 @@
 package com.postservice.postservice.controller;
 
+import com.postservice.postservice.model.Post;
 import com.postservice.postservice.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class PostController {
     @Autowired
     private PostService postService;
+
+    @PutMapping("/posts/{postId}")
+    public ResponseEntity<Post> updatePost(@RequestBody @Valid Post post, @PathVariable("postId") String postId){
+        return  new ResponseEntity<>(postService.updatePost(post,postId), HttpStatus.ACCEPTED);
+    }
 }
--- src/main/java/com/postservice/postservice/service/PostService.java
+++ src/main/java/com/postservice/postservice/service/PostService.java
@@ -1,15 +1,21 @@
 package com.postservice.postservice.service;
 
+import com.postservice.postservice.model.Post;
 import com.postservice.postservice.repo.PostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class PostService {
 @Autowired
     private PostRepo postRepo;
 
-
+    public Post updatePost(Post post, String postId){
+        post.setUpdatedAt(LocalDateTime.now());
+        return this.postRepo.save(post);
+    }
 
 
 }
