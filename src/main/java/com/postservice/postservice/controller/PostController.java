4bbb9d360438bd0118bf0f407f7516fc089836d4
senil <senila@maveric-systems.com>
2022-03-21 23:37:30 +0530

    added user

--- src/main/java/com/postservice/postservice/controller/PostController.java
+++ src/main/java/com/postservice/postservice/controller/PostController.java
@@ -1,11 +1,23 @@
 package com.postservice.postservice.controller;
 
+import com.postservice.postservice.model.Post;
 import com.postservice.postservice.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class PostController {
     @Autowired
     private PostService postService;
+
+    @PostMapping("/posts")
+    public ResponseEntity<Post> savePost(@RequestBody @Valid Post post){
+        return  new ResponseEntity<>(postService.savePost(post), HttpStatus.ACCEPTED);
+    }
 }
--- src/main/java/com/postservice/postservice/service/PostService.java
+++ src/main/java/com/postservice/postservice/service/PostService.java
@@ -1,14 +1,22 @@
 package com.postservice.postservice.service;
 
+import com.postservice.postservice.model.Post;
 import com.postservice.postservice.repo.PostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class PostService {
 @Autowired
     private PostRepo postRepo;
 
+    public Post savePost(Post post){
+        post.setCreatedAt(LocalDateTime.now());
+        post.setUpdatedAt(null);
+        return postRepo.save(post);
+    }
 
 
 
