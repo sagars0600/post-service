829725524c251554b723dba8dec4fdd2985c39bb
senil <senila@maveric-systems.com>
2022-03-22 15:37:46 +0530

    getAll post feature

--- src/main/java/com/postservice/postservice/service/PostService.java
+++ src/main/java/com/postservice/postservice/service/PostService.java
@@ -1,15 +1,20 @@
 package com.postservice.postservice.service;
 
+import com.postservice.postservice.model.Post;
 import com.postservice.postservice.repo.PostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostService {
 @Autowired
     private PostRepo postRepo;
 
-
+    public List<Post> showAll(){
+        return postRepo.findAll();
+    }
 
 
 }
